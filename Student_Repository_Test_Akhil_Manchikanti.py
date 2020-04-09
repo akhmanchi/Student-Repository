bca74d0c36852d7ade1ad8cd179dbdebb05981a3
Akhil Manchikanti <akhilm1995@gmail.com>
2020-04-08 21:54:49 -0400

    Add files via upload
    Added remaining courses and electives; GPA; Major table with required and electives

--- Student_Repository_Test_Akhil_Manchikanti.py
+++ Student_Repository_Test_Akhil_Manchikanti.py
@@ -5,23 +5,23 @@ class TestRepository(unittest.TestCase):
     """ Tests all the methods in HW09_Akhil_Manchikanti """
     def test_Student(self) -> None:
         """ Tests the student repository """
-        data: Repository = Repository("D:\MS\Stevens Institute of Technology\SSW810\Assignment9\Stevens")
+        data: Repository = Repository("D:\MS\Stevens Institute of Technology\SSW810\Assignment10\Stevens")
         student_data = {cwid: stud.info() for cwid, stud in data._students.items()}
-        expected = {'10103': ['10103','Baldwin, C',['CS 501','SSW 564', 'SSW 567', 'SSW 687']],
-                    '10115': ['10115','Wyatt, X',['CS 545','SSW 564', 'SSW 567', 'SSW 687']],
-                    '10172': ['10172','Forbes, I',['SSW 555', 'SSW 567']],
-                    '10175': ['10175','Erickson, D',['SSW 564', 'SSW 567', 'SSW 687']],
-                    '10183': ['10183','Chapman, O',['SSW 689']],
-                    '11399': ['11399','Cordova, I',['SSW 540']],
-                    '11461': ['11461','Wright, U',['SYS 611','SYS 750', 'SYS 800']],
-                    '11658': ['11658','Kelly, P',['SSW 540']],
-                    '11714': ['11714','Morton, A',['SYS 611','SYS 645']],
-                    '11788': ['11788','Fuller, E',['SSW 540']]}
+        expected = {'10103': ['10103', 'Baldwin, C', 'SFEN', ['CS 501', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], 3.44],
+                    '10115': ['10115', 'Wyatt, X', 'SFEN', ['CS 545', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], 3.81], 
+                    '10172': ['10172', 'Forbes, I', 'SFEN', ['SSW 555', 'SSW 567'], ['SSW 540', 'SSW 564'], ['CS 501', 'CS 513', 'CS 545'], 3.88], 
+                    '10175': ['10175', 'Erickson, D', 'SFEN', ['SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], ['CS 501', 'CS 513', 'CS 545'], 3.58], 
+                    '10183': ['10183', 'Chapman, O', 'SFEN', ['SSW 689'], ['SSW 540', 'SSW 555', 'SSW 564', 'SSW 567'],['CS 501', 'CS 513', 'CS 545'], 4.0], 
+                    '11399': ['11399', 'Cordova, I', 'SYEN', ['SSW 540'], ['SYS 612', 'SYS 671', 'SYS 800'], [], 3.0], 
+                    '11461': ['11461', 'Wright, U', 'SYEN', ['SYS 611', 'SYS 750', 'SYS 800'], ['SYS 612', 'SYS 671'], ['SSW 540', 'SSW 565', 'SSW 810'], 3.92],
+                    '11658': ['11658', 'Kelly, P', 'SYEN', [], ['SYS 612', 'SYS 671', 'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810'], 0], 
+                    '11714': ['11714', 'Morton, A', 'SYEN', ['SYS 611', 'SYS 645'], ['SYS 612', 'SYS 671', 'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810'], 3.0], 
+                    '11788': ['11788', 'Fuller, E', 'SYEN', ['SSW 540'],['SYS 612', 'SYS 671', 'SYS 800'], [], 4.0] }
         self.assertEqual(student_data, expected)
 
     def test_Instructor(self) -> None:
         """ Tests the instructor repository """
-        data: Repository = Repository("D:\MS\Stevens Institute of Technology\SSW810\Assignment9\Stevens")
+        data: Repository = Repository("D:\MS\Stevens Institute of Technology\SSW810\Assignment10\Stevens")
         instructor_data = {tuple(each_instructor) for inst in data._instructors.values() for each_instructor in inst.info()}
         expected = {('98765', 'Einstein, A', 'SFEN', 'SSW 567', 4),
                     ('98764', 'Feynman, R', 'SFEN', 'SSW 564', 3),
@@ -36,6 +36,15 @@ class TestRepository(unittest.TestCase):
                     ('98760', 'Darwin, C', 'SYEN', 'SYS 611', 2),
                     ('98760', 'Darwin, C', 'SYEN', 'SYS 645', 1)}
         self.assertEqual(instructor_data, expected)
+
+    def test_Major(self) -> None:
+        """ Tests the Major repository """
+        data: Repository = Repository("D:\MS\Stevens Institute of Technology\SSW810\Assignment10\Stevens")
+        major_data = {major: maj.info() for major, maj in data._majors.items()}
+        expected = {
+            'SFEN': ['SFEN', ['SSW 540', 'SSW 555', 'SSW 564', 'SSW 567'] ,['CS 501', 'CS 513', 'CS 545']],
+            'SYEN': ['SYEN', ['SYS 612', 'SYS 671', 'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810']]}
+        self.assertEqual(expected, major_data)
 
 if __name__ == "__main__":
     unittest.main(exit=False, verbosity=2)
